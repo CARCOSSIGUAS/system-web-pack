a27833340920886191ef8aeb051b5fc1855dd4d9
Ronald Torres <rtorres@technisys.com>
2022-08-23 11:04:07 -0500

    Fix dependencies

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,7 +1,7 @@
-//import React from "react";
-//import { default as BsButton } from "react-bootstrap/Button";
+import React from "react";
+import { default as BsButton } from "react-bootstrap/Button";
 import { ButtonType } from "./Button.types";
 
-const Button = ({ label }: ButtonType) => <div>{label}</div>;
+const Button = ({ label }: ButtonType) => <BsButton>{label}</BsButton>;
 
 export default Button;
